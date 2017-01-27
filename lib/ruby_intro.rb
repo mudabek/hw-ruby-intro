d9c825dad97d7a9fd77f5b17f90c346a8743784e
mudabek <nazarovo@grinnell.edu>
2017-01-27 00:18:29 -0600

    done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,79 @@
-# When done, submit this entire file to the autograder.
-
+# When done, submit this entire file to the autograder
+# worked with Prabir (pair-programming)
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |number| sum += number end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.sort!
+  if arr.length >= 1
+    sum += arr[-1]
+    if arr.length >= 2
+      sum += arr[-2]
+    end
+  end
+  return sum
 end
 
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  i = 0
+  val = false
+  for i in 0...arr.length
+    for j in i+1...arr.length 
+      if n == arr[i] + arr[j]
+        val = true
+        break
+      end 
+    end
+  end
+  return val
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return (s =~ /^(?![aeiou]).*/i) && (s =~ /^[a-z].*/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return (s =~ /(1*0*)*00$/) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize my_isbn, my_price
+      @isbn = my_isbn
+      @price = my_price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def isbn= val
+    @isbn = val
+  end
+  
+  def price
+    @price
+  end
+  
+  def price= val
+    @price = val
+  end
+  
+  def price_as_string 
+    return "$%.2f" % [@price]
+  end
 end
